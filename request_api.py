4c8294b984a6ecbd20f4b9e457e5ac227dfba703
antoine-lo <58790818+antoine-lo@users.noreply.github.com>
2022-04-14 18:30:05 +0200

    a lot of change

--- request_api.py
+++ request_api.py
@@ -3,17 +3,18 @@ import pandas as pd
 from Utils.conf import path_test
 import requests
 import json
+from os import environ, path
+from dotenv import load_dotenv
 
 
-"""df = pd.read_csv(path_test)
-url = 'http://127.0.0.1:5000/api/'
-id_input = "100001
-ID = int(id_input)
-x = df[df['SK_ID_CURR'] == ID]
-"""
+basedir = path.abspath(path.dirname(_file_))
+load_dotenv(path.join(basedir, '.env'))
+
 def api_result(x):
 
-    url = "http://127.0.0.1:5000/api/"
+  #  url = "http://127.0.0.1:5000/api/"
+  url=environ.get("url")
+
 
     j_data = x.to_json()
     headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
